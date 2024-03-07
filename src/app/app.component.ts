5c5f535bfb27baaf45d323fe1ab701ccbd4d7eb1
Ágata Alves <111840772+AgataPAlves@users.noreply.github.com>
2024-03-06 21:20:08 -0300

    front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DecimalPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
-  title = 'ang_calculator';
+    title = 'ang_calculator';
+    
+    calValue: number = 0;
+    funcT: any = 'NoFunction';
+
 }
