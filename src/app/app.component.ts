50b686fb351d56c5f523e9782873511a6789e9bb
Ágata Alves <111840772+AgataPAlves@users.noreply.github.com>
2024-03-07 22:34:10 -0300

    Backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,89 @@ export class AppComponent {
     calValue: number = 0;
     funcT: any = 'NoFunction';
 
+    calNumber: string = 'noValue';
+
+    firstNumber: number = 0;
+    secondNumber: number = 0;
+
+    onClickValue(val : string, type: any) {
+        console.log(val, type);
+        if (type == 'number') {
+            this.onNumberClick(val);
+        }
+        else if (type == 'function') { 
+            this.onFunctionClick(val);
+        }
+    }
+
+    onNumberClick(val : string){
+        if (this.calNumber != 'noValue') {
+            this.calNumber = this.calNumber + val;
+        }
+        else {
+            this.calNumber = val; 
+        }
+        this.calValue = parseFloat(this.calNumber);
+    }
+
+    onFunctionClick(val : string) {
+        if (this.funcT == 'NoFunction') {
+            this.firstNumber = this.calValue;
+            this.calValue = 0;
+            this.calNumber = 'noValue';
+            this.funcT = val;
+        }
+        else if (this.funcT != 'NoFunction') {
+            this.secondNumber = this.calValue;            
+            // calculation
+            this.valueCalculate(val);
+        }
+    }
+
+
+
+    valueCalculate(val: string) {
+        if (this.funcT == '+') {
+            const Total = this.firstNumber + this.secondNumber;
+            this.totalAssignValues(Total, val);
+            if (val == '=') {this.onEqualPress};
+        }
+        else if (this.funcT == '-') {
+            const Total = this.firstNumber - this.secondNumber;
+            this.totalAssignValues(Total, val);
+            if (val == '=') { this.onEqualPress };
+        }
+        else if (this.funcT == '*') {
+            const Total = this.firstNumber * this.secondNumber;
+            this.totalAssignValues(Total, val);
+            if (val == '=') { this.onEqualPress };
+        }
+        else if (this.funcT == '/') {
+            const Total = this.firstNumber / this.secondNumber;
+            this.totalAssignValues(Total, val);
+            if (val == '=') { this.onEqualPress };
+        }
+        else if (this.funcT == '%') {
+            const Total = this.calValue / 100;
+            console.log(Total);
+            this.onEqualPress();
+        }
+    }
+
+
+
+    totalAssignValues(Total: number, val: string) {
+        this.calValue = Total;
+        this.firstNumber = Total;
+        this.secondNumber = 0;
+        this.calNumber = 'noValue';
+        this.funcT = val;
+    }
+
+    onEqualPress() {
+        this.firstNumber = 0;
+        this.secondNumber = 0;
+        this.funcT = 'NoFunction';
+        this.calNumber = 'noValue';
+    }
 }
